0503fe3c71232bff21bb175e8da9360cfbed4155
Abhish13405 <abhishekkushwaha13405@gmail.com>
2026-07-31 15:50:27 +0530

    add reender

--- skillzo_frontend/src/api/axios.js
+++ skillzo_frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'https://skillzo-2-1-5.onrender.com'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://skillzo-2-1-5.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
