043e48f3f7977a6ab0672c7aa668fd638aa926c2
Abhish13405 <abhishekkushwaha13405@gmail.com>
2026-07-31 15:45:32 +0530

    add render

--- skillzo_backend/skillzo/settings.py
+++ skillzo_backend/skillzo/settings.py
@@ -98,9 +98,11 @@ MEDIA_ROOT = BASE_DIR / 'media'   # resumes, audio/video recordings stored here
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ---- CORS (React frontend on Vite dev server) ----
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://skillzo-2-1.vercel.app',
 ]
 
 # ---- DRF + JWT ----
--- skillzo_frontend/src/api/axios.js
+++ skillzo_frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE_URL = 'http://127.0.0.1:8000/api'
+export const API_BASE_URL = 'https://skillzo-2-1-5.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
